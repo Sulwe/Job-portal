5d9644024a86b8a2fc4095745e2ae1e85f52ed12
Sulwe <cypriansulwe.cs@gmail.com>
2024-06-30 14:17:41 +0300

    create job cards

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -1,15 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
+import Card from "../components/Card";
+import Jobs from "./Jobs";
 
 const Home = () => {
+    const [selectedCategory, setSelectedCategory] = useState(null)
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch("jobs.json").then(res => res.json()).then(data => {
+
+            setJobs(data)
+        })
+    
+    }, [])
+
+    console.log(jobs)
+
+    //handle input change
     const [query, setQuery] = useState("");
     const handleInputChange = (event) => {
         setQuery(event.target.value)
     }
-    console.log(query);
+
+    //filter jobs by title
+    const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    
+    //radio button filter
+    const handleChange = (event) => {
+        setSelectedCategory(event.target.value)
+    }
+
+    //button filter
+    const handleClick = (event) => {
+        setSelectedCategory(event.target.value)
+    }
+
+    //main function
+    const filteredData = (jobs, selected, query) => {
+        let filteredJobs = jobs;
+        if (query) {
+            filteredJobs = filteredItems;
+        }
+        //category filtering
+        if (selected) {
+            filteredJobs = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, 
+            employmentType, postingDate}) => (
+                jobLocation.toLowerCase() === selected.toLowerCase() ||
+                parseInt(maxPrice) <= parseInt(selected) ||
+                experienceLevel >= selected ||
+                salaryType.toLowerCase() === selected.toLowerCase() ||
+                employmentType.toLowerCase() === selected.toLowerCase() ||
+                postingDate >= selected
+            ));
+            console.log(filteredJobs)
+        }
+
+        return filteredJobs.map((data, i) => <Card key={i} data={data} />)
+    }
+
+    const result = filteredData(jobs, selectedCategory, query);
+
     return (
         <div>
             <Banner query = {query} handleInputChange = {handleInputChange}/>
+
+            {/* main content */}
+            <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
+                {/* left side */}
+                <div className="bg-white p-4 rounded">Left</div>
+                {/* job cards */}
+                <div className="col-span-2 bg-white p-4 rounded-sm">
+                    <Jobs result={result}/>
+                </div>
+                {/* right side */}
+                <div className="bg-white p-4 rounded">Right</div>
+
+            </div>
+
+            
         </div>
     )
 }
--- job-portal-client/src/components/Banner.jsx
+++ job-portal-client/src/components/Banner.jsx
@@ -29,8 +29,7 @@ const Banner = ({query, handleInputChange}) => {
                     focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3 w-full'>
                         <input type='text' name='title' id='title' placeholder='Location'
                         className=' block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 
-                        placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6' 
-                        value = {""}
+                        placeholder:text-gray-400 focus:right-0 sm:text-sm sm:leading-6'
                         />
                         <FiMapPin className='absolute mt-2.5 ml-2 text-gray-400'/>
 
